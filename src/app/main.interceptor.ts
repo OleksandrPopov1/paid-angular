8776d2df10548d7738a56d66abb494b4163213e2
Rams-Popov <sanchoss298@gmail.com>
2023-01-21 18:09:26 +0200

    update

---
+++ src/app/main.interceptor.ts
@@ -0,0 +1,68 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpInterceptor, HttpErrorResponse
+} from '@angular/common/http';
+import {catchError, Observable, switchMap, throwError} from 'rxjs';
+import {Router} from "@angular/router";
+
+import {AuthService} from "./modules/login/servisces";
+import {ITokens} from "./modules/login/interfacecs";
+
+@Injectable()
+export class MainInterceptor implements HttpInterceptor {
+  isRefreshing = false;
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<any> {
+    const isAuthenticated = this.authService.isAuthenticated();
+
+    if (isAuthenticated) {
+      request = this.addToken(request, this.authService.getAccessToken());
+    }
+
+    return next.handle(request).pipe(
+      catchError((res: HttpErrorResponse) => {
+
+        if (res && res.error && res.status === 401) {
+          return this.handle401Error(request, next);
+        }
+
+        return throwError(() => new Error('token invalid or expired'))
+      })
+    );
+  }
+
+
+  private addToken(request: HttpRequest<any>, token: string): HttpRequest<any> {
+    return request.clone({
+      setHeaders: {Authorization: `Bearer ${token}`}
+    })
+  }
+
+  private handle401Error(request: HttpRequest<any>, next: HttpHandler): any {
+    const refresh = this.authService.getRefreshToken();
+    if (!this.isRefreshing && refresh) {
+      this.isRefreshing = true
+      return this.authService.refresh(refresh).pipe(
+        switchMap((tokens: ITokens) => {
+          this.isRefreshing = false;
+          return next.handle(this.addToken(request, tokens.access));
+        }),
+        catchError(() => {
+          this.isRefreshing = false;
+          this.authService.deleteTokens();
+          this.router.navigate(['/login']);
+          return throwError(() => new Error('Token invalid or expired'));
+        })
+      )
+    }
+
+    this.router.navigate(['/login']);
+    return throwError(() => new Error('Not login'));
+  }
+
+}
---
+++ src/app/modules/login/components/login-form/login-form.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../servisces";
+import {Router} from "@angular/router";
+import {MatDialogRef} from "@angular/material/dialog";
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+  form: FormGroup;
+  error: boolean = false;
+  hidePassword = true;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private matDialogRef: MatDialogRef<LoginFormComponent>
+  ) {
+    this._initialForm();
+  }
+
+  ngOnInit(): void {
+  }
+
+  _initialForm(): void {
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  login(): void {
+    this.authService.login(this.form.value).subscribe({
+      next: () => {
+        this.error = false;
+        this.router.navigate(['/orders']);
+        this.matDialogRef.close();
+      },
+      error: () => {
+        this.error = true;
+        this.form.reset();
+      }
+    })
+  }
+}
